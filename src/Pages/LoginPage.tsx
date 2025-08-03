31d3c553b4c2f390d1aa376d61540a6bc03ff093
sky33-git <sunilsks2017@gmail.com>
2025-08-03 12:09:42 +0530

    test1

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -63,8 +63,7 @@ export default function LoginPage() {
         navigate('/role-selection');
       }
       else {
-        throw new Error('Unknown response from server');
-        
+        throw new Error('Unknown response from server: test123213');
       }
     }
     catch (error) {
